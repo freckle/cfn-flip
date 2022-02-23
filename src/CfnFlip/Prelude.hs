ad2d853eb290408fa9d1c3b92ffd66f2fc7b3b31
patrick brisbin <pbrisbin@gmail.com>
2022-02-23 12:16:42 -0500

    Remove RIO dependency

--- src/CfnFlip/Prelude.hs
+++ src/CfnFlip/Prelude.hs
@@ -1,6 +1,26 @@
 module CfnFlip.Prelude
   ( module X
+  , readFileBinary
+  , writeFileBinary
   ) where
 
-import RIO as X
-import RIO.Text as X (pack, unpack)
+import Prelude as X
+
+import Control.Monad as X (unless, when, (<=<))
+import Control.Monad.IO.Class as X (MonadIO(..))
+import Control.Monad.IO.Unlift as X (MonadUnliftIO)
+import Data.ByteString as X (ByteString)
+import Data.Foldable as X (for_, traverse_)
+import Data.Maybe as X (fromMaybe)
+import Data.Text as X (Text, pack, unpack)
+import Data.Text.Encoding as X (encodeUtf8)
+import Data.Word as X (Word8)
+import UnliftIO.Exception as X (Exception, throwIO, throwString)
+
+import qualified Data.ByteString as BS
+
+readFileBinary :: MonadIO m => FilePath -> m ByteString
+readFileBinary = liftIO . BS.readFile
+
+writeFileBinary :: MonadIO m => FilePath -> ByteString -> m ()
+writeFileBinary p = liftIO . BS.writeFile p
